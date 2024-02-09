8d03619a33ed949685f8e3f7bb992d08ba0ad8d8
SetroZ <=>
2024-02-09 09:10:33 +0800

    added typescript for lambda

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -6,6 +6,7 @@ import {
   Function as LambdaFunction,
   Runtime,
 } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { Construct } from 'constructs'
 import { join } from 'path'
 interface LambdaStackProps extends StackProps {
@@ -15,11 +16,10 @@ export class LambdaStack extends Stack {
   public readonly helloLambdaIntegration: LambdaIntegration
   constructor(scope: Construct, id: string, props?: LambdaStackProps) {
     super(scope, id, props)
-
-    const helloLambda = new LambdaFunction(this, 'HelloLambda', {
+    const helloLambda = new NodejsFunction(this, 'HelloLambda', {
       runtime: Runtime.NODEJS_18_X,
-      handler: 'hello.main',
-      code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
+      handler: 'handler',
+      entry: join(__dirname, '..', '..', 'services','hello.ts'),
       environment: {
         TABLE_NAME: props.spacesTable.tableName,
       },
