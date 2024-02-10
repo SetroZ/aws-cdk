1addc3b8add259d6dabed9ece436bfc95c03bd05
SetroZ <=>
2024-02-10 14:11:48 +0800

    added debug

--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -1,19 +1,30 @@
 import { ListBucketsCommand, S3Client } from '@aws-sdk/client-s3'
+import { HttpMethod } from 'aws-cdk-lib/aws-events'
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Context,
 } from 'aws-lambda'
-import { v4 } from 'uuid'
-const s3Client = new S3Client({}) 
-async function handler(event: APIGatewayProxyEvent, context: Context) {
+const s3Client = new S3Client({})
+async function handler(
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  switch ((event.httpMethod)) {
+    case 'GET':
+
+    case 'POST':
+
+    default:
+      break
+  }
   const command = new ListBucketsCommand({})
 
   const listBuckets = (await s3Client.send(command)).Buckets
 
   const response: APIGatewayProxyResult = {
     statusCode: 200,
-    body: JSON.stringify('Hello from lambda, this is the id' +listBuckets),
+    body: JSON.stringify('Hello from lambda, this is the id' + listBuckets),
   }
   console.log(event)
   console.log(context)
